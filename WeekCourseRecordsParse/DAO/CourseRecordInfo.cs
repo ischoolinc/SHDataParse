1ab3e7d5e1512aa6df87d8435243f3ae940bfdce
chuntaichen <ct.chen@ischool.com.tw>
2022-12-29 01:21:12 +0800

    課程週課表解析建置專案先簽版

---
+++ WeekCourseRecordsParse/DAO/CourseRecordInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekCourseRecordsParse.DAO
+{
+    public class CourseRecordInfo
+    {
+        public string CourseID { get; set; } // 課程系統編號
+        public string Week { get; set; } // 星期
+        public string Period { get; set; } // 節次
+        public string SchoolYear { get; set; } // 學年度
+        public string Semester { get; set; } // 學期
+        public string CourseName { get; set; } // 課程名稱
+        public string SubjectName { get; set; } // 科目
+        public string ClassName { get; set; } // 班級
+        public string TeacherName1 { get; set; } // 授課教師1
+
+    }
+}
---
+++ WeekCourseRecordsParse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FISCA.Permission;
+using FISCA;
+using FISCA.Presentation;
+using WeekCourseRecordsParse.winForm;
+
+namespace WeekCourseRecordsParse
+{
+    public class Program
+    {
+        [FISCA.MainMethod()]
+        public static void main()
+        {
+            RibbonBarItem rbItem1 = MotherForm.RibbonBarItems["課程", "資料統計"];
+            rbItem1["解析"]["課程相關解析"]["課程週課表解析"].Enable = UserAcl.Current["5B05FBB6-45D1-4FDD-BAE9-7A446986FCDE"].Executable;
+            rbItem1["解析"]["課程相關解析"]["課程週課表解析"].Click += delegate
+            {
+                MainForm mf = new MainForm();
+                mf.ShowDialog();
+            };
+
+            // 權限註冊
+            Catalog catalog1 = RoleAclSource.Instance["課程"]["功能按鈕"];
+            catalog1.Add(new RibbonFeature("5B05FBB6-45D1-4FDD-BAE9-7A446986FCDE", "課程週課表解析"));
+        }
+    }
+}
