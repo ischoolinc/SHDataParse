7859c1dc9ac75b9be4a5b39b5ff21055fa865aa3
Bo-Yen Wang <jackie2014@gmail.com>
2023-09-11 12:16:35 +0800

    週課表解析新增自訂解析功能

--- WeekCourseRecordsParse/DAO/CourseRecordInfo.cs
+++ WeekCourseRecordsParse/DAO/CourseRecordInfo.cs
@@ -15,11 +15,31 @@ namespace WeekCourseRecordsParse.DAO
         public string Semester { get; set; } // 學期
         public string CourseName { get; set; } // 課程名稱
         public string SubjectName { get; set; } // 科目
+        public string SubjectLevel { get; set; } // 科目級別
         public string ClassName { get; set; } // 班級
+        public string ClassNumber { get; set; } // 班級代碼
         //public string TeacherName1 { get; set; } // 授課教師1
 
          // 存授課教師 1,2,3
         public List<string> TeacherNameList { get; set; }
 
+    }
+    public class CourseFiledsMap
+    {
+        public string fieldName { get; set; }
+        public string ref_filedName { get; set; }
+        public bool isForCompare { get; set; }
+        public CourseFiledsMap(string fieldName, string ref_filedName, bool isForCompare)
+        {
+            this.fieldName = fieldName;
+            this.ref_filedName = ref_filedName;
+            this.isForCompare = isForCompare;
+        }
+        public CourseFiledsMap() 
+        {
+            this.fieldName = "";
+            this.ref_filedName = "";
+            this.isForCompare = false;
+        }
     }
 }
--- WeekCourseRecordsParse/winForm/MainForm.cs
+++ WeekCourseRecordsParse/winForm/MainForm.cs
@@ -17,6 +17,7 @@ namespace WeekCourseRecordsParse.winForm
     {
         string SchoolYear = "";
         string Semester = "";
+        public Dictionary<string, CourseFiledsMap> courseDic= new Dictionary<string, CourseFiledsMap>();
 
         public MainForm()
         {
@@ -31,8 +32,19 @@ namespace WeekCourseRecordsParse.winForm
             SchoolYear = K12.Data.School.DefaultSchoolYear;
             Semester = K12.Data.School.DefaultSemester;
 
+            loadCourseFiledMap();
+
             // 畫面值
             labelSchoolYear.Text = $"{SchoolYear}學年度第{Semester}學期";
+
+        }
+
+        private void loadCourseFiledMap()
+        {
+            courseDic.Add("科目名稱", new CourseFiledsMap());
+            courseDic.Add("班級名稱", new CourseFiledsMap());
+            courseDic.Add("班碼名稱", new CourseFiledsMap());
+            courseDic.Add("教師名稱", new CourseFiledsMap());
         }
 
         private void buttonParse01_Click(object sender, EventArgs e)
@@ -62,7 +74,7 @@ namespace WeekCourseRecordsParse.winForm
 
         private void buttonEnable(bool value)
         {
-            buttonParse01.Enabled = buttonParse02.Enabled = buttonParse03.Enabled = value;
+            buttonParse01.Enabled = buttonParse02.Enabled = buttonParse03.Enabled = btnAnalysis.Enabled=value;
         }
 
         private void btnExit_Click(object sender, EventArgs e)
@@ -107,6 +119,71 @@ namespace WeekCourseRecordsParse.winForm
 
                 //  解析資料
                 Workbook resultWb = DataTransfer.CourseDataParse03(wb, CourseRecordList);
+
+                // 輸出
+                Utility.ExportXls(SchoolYear + "學年度第" + Semester + "學期_課程週課表解析", resultWb);
+
+            }
+            else
+            {
+                MsgBox.Show("讀取 Excel 檔案發生錯誤或版本過舊，要Excel 97以上版本。");
+            }
+
+            buttonEnable(true);
+        }
+
+        private void btnAnalysis_Click(object sender, EventArgs e)
+        {
+            buttonEnable(false);
+
+            bool hasChecked=false;
+
+            //設定比對欄位與比對欄位名稱
+            if(chkSubjectName.Checked)
+            {
+                hasChecked = true;
+                courseDic["科目名稱"].isForCompare = true;
+            }
+            courseDic["科目名稱"].fieldName = "科目名稱";
+            courseDic["科目名稱"].ref_filedName = txtSubjectName.Text;
+            if (chkClassName.Checked)
+            {
+                hasChecked = true;
+                courseDic["班級名稱"].isForCompare = true;
+            }
+            courseDic["班級名稱"].fieldName = "班級名稱";
+            courseDic["班級名稱"].ref_filedName = txtClassName.Text;
+            if (chkClassNumber.Checked)
+            {
+                hasChecked = true;
+                courseDic["班碼名稱"].isForCompare = true;
+            }
+            courseDic["班碼名稱"].fieldName = "班碼名稱";
+            courseDic["班碼名稱"].ref_filedName = txtClassNumber.Text;
+            if (chkTeacherName.Checked)
+            {
+                hasChecked = true;
+                courseDic["教師名稱"].isForCompare = true;
+            }
+            courseDic["教師名稱"].fieldName = "教師名稱";
+            courseDic["教師名稱"].ref_filedName = txtTeacherName.Text;
+
+            if(!hasChecked)
+            {
+                MsgBox.Show("請勾選自訂比對欄位");
+                buttonEnable(true);
+                return;
+            }
+
+            // 讀取解析檔
+            Workbook wb = Utility.ReadXlsFile(true);
+            if (wb != null)
+            {
+                // 讀取課程資料
+                List<CourseRecordInfo> CourseRecordList = DataTransfer.GetCourseRecordListBySchoolYearSemester(SchoolYear, Semester);
+
+                //  解析資料
+                Workbook resultWb = DataTransfer.CourseDataParseX(wb, CourseRecordList,courseDic,txtSeparator.Text,chkCourseTeacher.Checked);
 
                 // 輸出
                 Utility.ExportXls(SchoolYear + "學年度第" + Semester + "學期_課程週課表解析", resultWb);
